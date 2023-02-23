b253bf9c7def22343ec610c6d55f3f8fd8362ff0
vadim l <vadimlozinschii@gmail.com>
2023-02-23 11:42:27 -0800

    created an event listener function on click

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+const m1 = document.getElementById('button');
+
+function addingEventListener(e) {
+    alert('I was clicked!');
+    m1.addEventListener('click', addingEventListener);
 }
+
+// m1.addEventListener('click', addingEventListener);
